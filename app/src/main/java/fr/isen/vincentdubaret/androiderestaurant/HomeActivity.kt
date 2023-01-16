ac406844c885bf821e88bc8f1f180cdc051cf555
chinkara <poubelledechinkara@outlook.com>
2023-01-16 12:00:03 +0100

    final layout and toast test

--- app/src/main/java/fr/isen/vincentdubaret/androiderestaurant/HomeActivity.kt
+++ app/src/main/java/fr/isen/vincentdubaret/androiderestaurant/HomeActivity.kt
@@ -1,11 +1,18 @@
 package fr.isen.vincentdubaret.androiderestaurant
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        //set available choices for smoke, light and sound
+        val button_dessert = findViewById<Button>(R.id.button_dessert)
+        Toast.makeText(applicationContext, "Hello toast!", Toast.LENGTH_SHORT).show()
+
     }
 }

f6d2eee32e32349017c7a708f9085d778aab71a8
chinkara <poubelledechinkara@outlook.com>
2023-01-17 12:04:28 +0100

    post volley implementation, working but not to the correct URL

--- app/src/main/java/fr/isen/vincentdubaret/androiderestaurant/MealListActivity.kt
+++ app/src/main/java/fr/isen/vincentdubaret/androiderestaurant/MealListActivity.kt
@@ -2,9 +2,17 @@ package fr.isen.vincentdubaret.androiderestaurant
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import fr.isen.vincentdubaret.androiderestaurant.databinding.ActivityMealListBinding
+import org.json.JSONArray
+import org.json.JSONObject
+import java.nio.charset.Charset
+
 
 class MealListActivity : AppCompatActivity() {
 
@@ -37,6 +45,35 @@ class MealListActivity : AppCompatActivity() {
             val myIntent = Intent(this@MealListActivity, HomeActivity::class.java)
             startActivity(myIntent)
         }
+
+        val queue = Volley.newRequestQueue(this)
+        //val url = "http://test.api.catering.bluecodegames.com/menu"
+        val url = "https://private-4c0e8-simplestapi3.apiary-mock.com/message"
+
+        //val jsonArray = JSONArray()
+        //val jsonObject = JSONObject()
+        //jsonObject.put("id_sho", 1)
+        //jsonArray.put(jsonObject)
+        //val requestBody = jsonObject.toString()
+
+        val requestBody = "id_sho=1"
+        //val requestBody = "id=1" + "&msg=test_msg"
+        val stringReq : StringRequest =
+            object : StringRequest(Method.POST, url,
+                Response.Listener { response ->
+                    // response
+                    var strResp = response.toString()
+                    Log.d("API", strResp)
+                },
+                Response.ErrorListener { error ->
+                    Log.d("API", "error => $error")
+                }
+            ){
+                override fun getBody(): ByteArray {
+                    return requestBody.toByteArray(Charset.defaultCharset())
+                }
+            }
+        queue.add(stringReq)
     }
     private fun myOnClickItem(mealName: String){
         //Log.d("##########HUMAN############", message)
